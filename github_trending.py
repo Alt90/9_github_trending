d974303911cbe5efec23cefaf4cf9fcbbdbe7bff
Alt13 <Alt90@rambler.ru>
2016-10-11 10:35:16 +0300

    Переделал функцию определение жаты создания рапазитариев

--- github_trending.py
+++ github_trending.py
@@ -4,17 +4,17 @@ from datetime import date
 from datetime import timedelta
 
 
-def get_last_week_data():
+def get_last_data(last_weeks=1, last_day=0):
     today = date.today()
-    return today - timedelta(weeks=1)
+    return today - timedelta(weeks=last_weeks, days=last_day)
 
 
 def get_trending_repositories(top_size=20):
     r = requests.get('https://api.github.com/search/repositories',
-                     params={'q': 'created:>%s' % get_last_week_data(),
+                     params={'q': 'created:>%s' % get_last_data(),
                              'sort': 'stars',
                              'order': 'desc'})
-    return list(r.json()['items'])[:top_size]
+    return r.json()['items'][:top_size]
 
 
 def print_trending_repositories(trending_repositories):
